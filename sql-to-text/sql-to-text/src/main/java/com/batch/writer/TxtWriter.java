c3b04892168f57d7a450b99ae7c665d8a87110c5
BhavanaPaedraju <91584031+BhavanaPaedraju@users.noreply.github.com>
2022-01-21 16:46:33 +0530

    Add files via upload

---
+++ sql-to-text/sql-to-text/src/main/java/com/batch/writer/TxtWriter.java
@@ -0,0 +1,20 @@
+package com.batch.writer;
+
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.core.io.ClassPathResource;
+
+import com.batch.model.Person;
+
+public class TxtWriter extends FlatFileItemWriter<Person>{
+    public TxtWriter() {
+        this.setResource(new ClassPathResource("person.txt"));
+        DelimitedLineAggregator<Person> lineAggregator=new DelimitedLineAggregator<Person>();
+        lineAggregator.setDelimiter(",");
+        BeanWrapperFieldExtractor<Person> fieldExtractor=new BeanWrapperFieldExtractor<Person>();
+        fieldExtractor.setNames(new String[]{"id","name"});
+        lineAggregator.setFieldExtractor(fieldExtractor);
+        this.setLineAggregator(lineAggregator);
+    }
+}
